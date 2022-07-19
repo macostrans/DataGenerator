3232740c3043888ba918cd5ad1e10e1a2a0cacb6
Kannan Thangaraju <kannanoferode@gmail.com>
2022-07-19 18:38:03 +0530

    moved labels to deploy

--- consolidated/manager-traefik-node-initialization-commands.sh
+++ consolidated/manager-traefik-node-initialization-commands.sh
@@ -4,3 +4,7 @@ docker network create --driver=overlay traefik-public
 export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 docker node update --label-add traefik-public.traefik-public-certificates=true $NODE_ID
 export EMAIL=kannanoferode@gmail.com
+export DOMAIN=datadeveloperfusion.com
+export USERNAME=admin
+export PASSWORD=password
+export HASHED_PASSWORD=$(openssl passwd -apr1 $PASSWORD)
