96970a8347c4bc28059f57db19c6b5cdc286257a
Kannan Thangaraju <kannanoferode@gmail.com>
2022-07-21 13:38:15 +0530

    gitlab-runner files

---
+++ consolidated/gitlab-runner-intialization-commans.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
+dpkg -i gitlab-runner_amd64.deb
+sudo gitlab-runner register
+sudo usermod -aG dockergitlab-runner
+sudo nano /etc/sudoers
